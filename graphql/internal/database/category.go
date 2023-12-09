b83fefc8c3ccf1ee6585c534f2be3e78310c0367
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-12-09 19:15:23 -0300

    feat(graphql): add course database resolvers

--- graphql/internal/database/category.go
+++ graphql/internal/database/category.go
@@ -23,14 +23,33 @@ func (c *Category) Create(name string, description string) (Category, error) {
 		"INSERT INTO categories (id, name, description) VALUES ($1, $2, $3)",
 		id, name, description,
 	)
-
 	if err != nil {
 		return Category{}, err
 	}
-
 	return Category{
 		ID:          id,
 		Name:        name,
 		Description: description,
 	}, nil
+}
+
+func (c *Category) FindaAll() ([]Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	categories := []Category{}
+	for rows.Next() {
+		var id, name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, Category{
+			ID:          id,
+			Name:        name,
+			Description: description,
+		})
+	}
+	return categories, nil
 }
