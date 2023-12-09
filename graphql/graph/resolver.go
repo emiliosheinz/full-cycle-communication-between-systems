99e82a32f0a2bac2d7d0e2f9327317c411a1ea29
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-12-09 17:17:00 -0300

    feat(graphql): add CreateCategory mutation

--- graphql/graph/resolver.go
+++ graphql/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/emiliosheinz/full-cycle-cbs-graphql/internal/database"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	CategoryDb *database.Category
+}
